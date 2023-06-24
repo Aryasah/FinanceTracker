93e0d92952cdd7946f8bc32f7d431382bb02695d
arya <aryasah30@gmail.com>
2023-06-24 14:04:36 +0530

    finance tracker

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Ecommerce } from './Ecommerce';
+export { default as Customers } from './Customers';
+export { default as Pie } from './Charts/Pie';
+export { default as Stacked } from './Charts/Stacked';
+
